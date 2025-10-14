b1857822f85a703c7f639086141a81c7e65ab2ed
Andrea Grandi <andrea.grandi@tiscali.it>
2025-10-14 21:06:28 +0200

    base code for supervisor

--- src/magnet/supervisor.py
+++ src/magnet/supervisor.py
@@ -2,34 +2,37 @@ from typing import Any, List, Optional, Union
 from langgraph.graph.state import StateGraph, CompiledStateGraph
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.store.memory import InMemoryStore
-from langgraph_swarm import create_swarm
+from langgraph_supervisor import create_supervisor
 
 
-class Swarm:
+class Supervisor:
     def __init__(
         self, 
+        model: str,
+        prompt: str,
         agents: Union[List[CompiledStateGraph], CompiledStateGraph], 
         default_active_agent: str,
     ) -> None:
-        """Initialize a swarm with the given parameters."""
+        """Initialize a supervisor with the given parameters."""
 
         self.agents = agents
-        self.default_active_agent = default_active_agent
+        self.model = model
+        self.prompt = prompt
 
     def create(self) -> StateGraph:
-        """Build and return the swarm using the provided configuration."""
+        """Build and return the supervisor using the provided configuration."""
 
-        return create_swarm(
+        return create_supervisor(
             agents=self.agents, # type: ignore
-            default_active_agent=self.default_active_agent
+            model=self.model, # type: ignore
+            prompt=self.prompt
         )
     
     def compile(self) -> CompiledStateGraph:
-        """Compile the swarm into a runnable state graph with optional checkpointing."""
+        """Compile the supervisor into a runnable state graph with optional checkpointing."""
 
         checkpointer = InMemorySaver() # short-term memory
         store = InMemoryStore() # long-term memory
 
         return self.create().compile(checkpointer=checkpointer, store=store)
-
 
